b3bf09f385900c61c393de7269ba3505dd6f51db
avinashmanda28 <avinashmanda28@gmail.com>
2026-03-12 20:07:59 +0530

    Update controller.py

--- backend/core/controller.py
+++ backend/core/controller.py
@@ -23,6 +23,7 @@ from backend.modules.memory.memory_engine import MemoryEngine
 from backend.modules.orchestrator.orchestrator_engine import OrchestratorEngine
 from backend.modules.plugins.plugin_manager import PluginManager
 from backend.modules.router.model_router import ModelRouter
+from backend.modules.queue.task_queue import TaskQueue
 from backend.modules.composer.scene_composer import SceneComposer
 from backend.modules.editor.video_editor import VideoEditor
 from backend.modules.renderer.video_renderer import VideoRenderer
@@ -40,6 +41,7 @@ class Controller:
         self.orchestrator = OrchestratorEngine()
         self.plugins = PluginManager()
         self.router = ModelRouter()
+        self.queue = TaskQueue()
 
         self.prompt_interpreter = PromptInterpreter()
         self.style_engine = StyleEngine()
@@ -70,47 +72,30 @@ class Controller:
 
         try:
 
-            self.orchestrator.clear_pipeline()
-
-            prompt_model = self.router.get_model("prompt")
-
             prompt_data = self.prompt_interpreter.interpret(prompt)
 
-            style_model = self.router.get_model("style")
-
             style = self.style_engine.determine_style(prompt_data)
-
-            knowledge_model = self.router.get_model("knowledge")
 
             knowledge = self.knowledge_engine.collect_knowledge(prompt_data)
 
             video_plan = self.video_planner.create_plan(prompt_data)
 
-            script_model = self.router.get_model("script")
-
             script = self.script_generator.generate_script(video_plan)
-
-            story = self.story_engine.optimize_story(script)
-
-            emotions = self.emotion_engine.analyze_emotions(script)
-
-            hook = self.hook_detector.detect_hook(script)
-
-            captions = self.caption_generator.generate_captions(script)
 
             scenes = self.scene_splitter.split_scenes(script)
 
-            motion = self.motion_engine.generate_motion_plan(scenes)
+            # Parallel tasks
+            self.queue.clear_tasks()
 
-            editing = self.smart_editor.analyze_editing(script, scenes)
+            self.queue.add_task(self.visual_generator.generate_visuals, scenes)
+            self.queue.add_task(self.voice_generator.generate_voice, scenes)
+            self.queue.add_task(self.image_generator.generate_images, scenes)
 
-            broll = self.broll_engine.generate_broll(scenes)
+            self.queue.run_all()
 
             visuals = self.visual_generator.generate_visuals(scenes)
 
             voice_tracks = self.voice_generator.generate_voice(scenes)
-
-            optimized_audio = self.silence_remover.remove_silence(voice_tracks)
 
             images = self.image_generator.generate_images(visuals)
 
@@ -129,34 +114,15 @@ class Controller:
             self.assets.cleanup_assets()
 
             workflow = {
-                "prompt_model": prompt_model,
-                "style_model": style_model,
-                "knowledge_model": knowledge_model,
-                "script_model": script_model,
                 "prompt_analysis": prompt_data,
                 "style": style,
                 "knowledge": knowledge,
                 "video_plan": video_plan,
                 "script": script,
-                "story": story,
-                "emotions": emotions,
-                "hook": hook,
-                "captions": captions,
                 "scenes": scenes,
-                "motion": motion,
-                "editing_plan": editing,
-                "broll": broll,
-                "visual_prompts": visuals,
-                "voice_tracks": voice_tracks,
-                "optimized_audio": optimized_audio,
-                "images": images,
-                "scene_videos": composed_scenes,
-                "timeline": timeline,
                 "rendered_video": rendered_video,
                 "quality_scores": quality,
                 "suggested_improvements": improvements,
-                "pipeline": self.orchestrator.get_pipeline(),
-                "plugins_loaded": self.plugins.list_plugins(),
                 "system_health": health
             }
 
@@ -169,4 +135,4 @@ class Controller:
             return {
                 "error": str(e),
                 "system_health": self.healing.check_system_health()
-    }
+}
