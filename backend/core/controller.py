43b6054f8197722eadc468ad96546e6c68bd253e
avinashmanda28 <avinashmanda28@gmail.com>
2026-03-12 18:15:30 +0530

    Update controller.py

--- backend/core/controller.py
+++ backend/core/controller.py
@@ -1,22 +1,25 @@
+from backend.modules.prompt.prompt_interpreter import PromptInterpreter
+
+
 class Controller:
 
     def __init__(self):
-        self.name = "OAm² Controller"
+
+        self.prompt_interpreter = PromptInterpreter()
 
     def process_prompt(self, prompt: str):
 
+        prompt_data = self.prompt_interpreter.interpret(prompt)
+
         workflow = {
-            "prompt": prompt,
-            "steps": [
-                "prompt_interpreter",
+            "prompt_analysis": prompt_data,
+            "pipeline": [
                 "video_planner",
                 "script_generator",
                 "scene_splitter",
-                "visual_generator",
-                "voice_generator",
-                "scene_composer",
-                "video_composer",
-                "renderer"
+                "media_generation",
+                "editing_engine",
+                "render_engine"
             ]
         }
 
