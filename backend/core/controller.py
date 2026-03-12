bcb671e3a6d8c9f938829d16518d1f58026a6df7
avinashmanda28 <avinashmanda28@gmail.com>
2026-03-12 20:10:22 +0530

    Update controller.py

--- backend/core/controller.py
+++ backend/core/controller.py
@@ -24,6 +24,7 @@ from backend.modules.orchestrator.orchestrator_engine import OrchestratorEngine
 from backend.modules.plugins.plugin_manager import PluginManager
 from backend.modules.router.model_router import ModelRouter
 from backend.modules.queue.task_queue import TaskQueue
+from backend.modules.supervisor.agent_supervisor import AgentSupervisor
 from backend.modules.composer.scene_composer import SceneComposer
 from backend.modules.editor.video_editor import VideoEditor
 from backend.modules.renderer.video_renderer import VideoRenderer
@@ -32,6 +33,8 @@ from backend.modules.renderer.video_renderer import VideoRenderer
 class Controller:
 
     def __init__(self):
+
+        self.supervisor = AgentSupervisor()
 
         self.healing = SelfHealingSystem()
         self.assets = AssetManager()
@@ -68,46 +71,112 @@ class Controller:
         self.video_editor = VideoEditor()
         self.video_renderer = VideoRenderer()
 
+    def safe_run(self, name, func, *args):
+
+        try:
+
+            self.supervisor.register_agent(name)
+
+            result = func(*args)
+
+            return result
+
+        except Exception:
+
+            self.supervisor.report_failure(name)
+
+            self.supervisor.restart_agent(name)
+
+            return None
+
     def process_prompt(self, prompt: str):
 
         try:
 
-            prompt_data = self.prompt_interpreter.interpret(prompt)
+            prompt_data = self.safe_run(
+                "prompt_interpreter",
+                self.prompt_interpreter.interpret,
+                prompt
+            )
 
-            style = self.style_engine.determine_style(prompt_data)
+            style = self.safe_run(
+                "style_engine",
+                self.style_engine.determine_style,
+                prompt_data
+            )
 
-            knowledge = self.knowledge_engine.collect_knowledge(prompt_data)
+            knowledge = self.safe_run(
+                "knowledge_engine",
+                self.knowledge_engine.collect_knowledge,
+                prompt_data
+            )
 
-            video_plan = self.video_planner.create_plan(prompt_data)
+            video_plan = self.safe_run(
+                "video_planner",
+                self.video_planner.create_plan,
+                prompt_data
+            )
 
-            script = self.script_generator.generate_script(video_plan)
+            script = self.safe_run(
+                "script_generator",
+                self.script_generator.generate_script,
+                video_plan
+            )
 
-            scenes = self.scene_splitter.split_scenes(script)
+            scenes = self.safe_run(
+                "scene_splitter",
+                self.scene_splitter.split_scenes,
+                script
+            )
 
-            # Parallel tasks
-            self.queue.clear_tasks()
+            visuals = self.safe_run(
+                "visual_generator",
+                self.visual_generator.generate_visuals,
+                scenes
+            )
 
-            self.queue.add_task(self.visual_generator.generate_visuals, scenes)
-            self.queue.add_task(self.voice_generator.generate_voice, scenes)
-            self.queue.add_task(self.image_generator.generate_images, scenes)
+            voice_tracks = self.safe_run(
+                "voice_generator",
+                self.voice_generator.generate_voice,
+                scenes
+            )
 
-            self.queue.run_all()
+            images = self.safe_run(
+                "image_generator",
+                self.image_generator.generate_images,
+                visuals
+            )
 
-            visuals = self.visual_generator.generate_visuals(scenes)
+            composed_scenes = self.safe_run(
+                "scene_composer",
+                self.scene_composer.compose_scenes,
+                images,
+                voice_tracks
+            )
 
-            voice_tracks = self.voice_generator.generate_voice(scenes)
+            timeline = self.safe_run(
+                "video_editor",
+                self.video_editor.assemble_video,
+                composed_scenes
+            )
 
-            images = self.image_generator.generate_images(visuals)
+            rendered_video = self.safe_run(
+                "video_renderer",
+                self.video_renderer.render_video,
+                timeline
+            )
 
-            composed_scenes = self.scene_composer.compose_scenes(images, voice_tracks)
+            quality = self.safe_run(
+                "quality_engine",
+                self.quality_engine.evaluate_video,
+                rendered_video
+            )
 
-            timeline = self.video_editor.assemble_video(composed_scenes)
-
-            rendered_video = self.video_renderer.render_video(timeline)
-
-            quality = self.quality_engine.evaluate_video(rendered_video)
-
-            improvements = self.improvement_engine.analyze_improvements(quality)
+            improvements = self.safe_run(
+                "improvement_engine",
+                self.improvement_engine.analyze_improvements,
+                quality
+            )
 
             health = self.healing.check_system_health()
 
@@ -123,6 +192,7 @@ class Controller:
                 "rendered_video": rendered_video,
                 "quality_scores": quality,
                 "suggested_improvements": improvements,
+                "agent_status": self.supervisor.get_status(),
                 "system_health": health
             }
 
